68c012b0e714f58e2e8e5571005ae29e768c2f9c
Yassin Eldeeb <72823042+YassinEldeeb@users.noreply.github.com>
2021-06-08 19:20:01 +0200

    removing files from src

--- Scripts/Fullstack.js
+++ Scripts/Fullstack.js
@@ -4,14 +4,14 @@ import { spawn } from 'child_process'
 import modfiyPackage from '../utils/modifyPackage.js'
 
 const FullStackApp = async () => {
-  const srcDir = `./src/backendBoilerplate`
+  const srcDir = `./backendBoilerplate`
   const destDir = `./backend`
 
   if (!fs.existsSync(destDir)) {
     try {
       await fs.copy(srcDir, destDir)
       await fs.rename(`${destDir}/.env`, './.env')
-      await fs.copy(`./src/Scripts/Readme.md`, './Readme.md')
+      await fs.copy(`./Scripts/Readme.md`, './Readme.md')
       spawn(
         `npm init -y && npm i express mongoose nodemon concurrently dotenv colors`,
         [],
